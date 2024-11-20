43271445a02fde6edbf0a1e8c244f09ad700adad
virimine <virimine@gmail.com>
2024-11-20 23:44:39 +0100

    (SAVE) Added Avoiding Logic and ReTrained || Trained first Prey

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wall : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Wall : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
