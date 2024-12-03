0a33206fa7d01827e5291f8e000431f9a48bd65e
virimine <virimine@gmail.com>
2024-12-03 19:05:21 +0100

    Final Training

--- Assets/Scripts/Predator.cs
+++ Assets/Scripts/Predator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
@@ -7,79 +5,105 @@ using Unity.MLAgents.Sensors;
 
 public class Predator : Agent {
 
-	[SerializeField] Transform preyTransform;
+	[SerializeField] Prey prey;
+	[SerializeField] TargetPrey_Test preyTest;
 	[SerializeField] float moveSpeed = 1f;
+	[SerializeField] float minDistanceMoved = 0.1f;
 	[SerializeField] Material winMaterial;
 	[SerializeField] Material loseMaterial;
 	[SerializeField] Transform predatorSpawnArea;
 	[SerializeField] Transform preySpawnArea;
 
-	Vector3 initialPos;
-	Vector3 preyInitialPos;
+	Vector3 lastPosition = new Vector3();
 	MeshRenderer meshRenderer;
+	Material defaultMaterial;
 
 	void Awake() => meshRenderer = GetComponent<MeshRenderer>();
 
-	//void Start() {
-	//	initialPos = transform.localPosition;
-	//	preyInitialPos = preyTransform.localPosition;
-	//}
+	void Start() => defaultMaterial = GetComponent<MeshRenderer>().material;
+
+	// Initialize episode.
+	public override void OnEpisodeBegin() {
+		transform.localPosition = GetInitialPosition(predatorSpawnArea);
+		prey.transform.localPosition = GetInitialPosition(preySpawnArea);
+	}
 
 	Vector3 GetInitialPosition(Transform spawnArea) {
 		var rndSpawnBounds = new Vector3(Random.Range((-spawnArea.localScale.x / 2), spawnArea.localScale.x / 2), 0, Random.Range((-spawnArea.localScale.z / 2), spawnArea.localScale.z / 2));
 		return spawnArea.localPosition + rndSpawnBounds;
 	}
 
-	// Initialize episode.
-	public override void OnEpisodeBegin() {
-		transform.localPosition = GetInitialPosition(predatorSpawnArea);
-		preyTransform.localPosition = GetInitialPosition(preySpawnArea);
-
-		//transform.localPosition = initialPos;
-		//preyTransform.localPosition = preyInitialPos;
-
-		Debug.Log("reset");
-	}
-
 	// Adds the agent's position and the target's position to the sensor for observation.
 	public override void CollectObservations(VectorSensor sensor) {
 		sensor.AddObservation(transform.localPosition);
-		sensor.AddObservation(preyTransform.localPosition);
+		sensor.AddObservation(prey.transform.localPosition);
 	}
 
 	// Process the continuous actions (movement along x and z axes) and applies them to move the agent. Called when the agent receives an action during the simulation.
 	public override void OnActionReceived(ActionBuffers actions) {
-
-		//Debug.Log(actions.DiscreteActions[0]);
 
 		var x = actions.ContinuousActions[0];
 		var z = actions.ContinuousActions[1];
 		var moveDirection = new Vector3(x, 0, z);
 
 		transform.localPosition += moveDirection * Time.deltaTime * moveSpeed;
+		
+		RewardAgression();
+
+		if (StepCount == MaxStep) {
+			meshRenderer.material = defaultMaterial;
+			prey.OnEvaded();
+			AddReward(-1f);
+		}
 	}
 
 	// Checks whether the agent has collided with a wall or the prey and ends the training episode.
-	void OnTriggerEnter(Collider other) {
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.layer == LayerMask.NameToLayer("Wall")) {
+			AddReward(-0.6f);
+			meshRenderer.material = loseMaterial;
+			prey.AddReward(0.001f);
 
-		if (other.TryGetComponent<Prey>(out Prey prey)) {
+			Debug.Log("PREDATOR HIT WALL");
+			EndEpisode();
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == LayerMask.NameToLayer("Prey")) {
+			prey.OnCaptured();
 			AddReward(1f);
 			meshRenderer.material = winMaterial;
 
-		}
-		else if (other.TryGetComponent<Wall>(out Wall wall)) {
-			AddReward(-1f);
-			meshRenderer.material = loseMaterial;
-		}
+			Debug.Log("GOTCHA!");
 			EndEpisode();
+		}
 	}
 
-	// For testing
-	public override void Heuristic(in ActionBuffers actionsOut) {
-		ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-		continuousActions[0] = Input.GetAxisRaw("Horizontal");
-		continuousActions[1] = Input.GetAxisRaw("Vertical");
+	void RewardAgression() {
+		var distanceFromPrey = Vector3.Distance(transform.position, prey.transform.position); // Calculate distance to prey
+		var distanceFromLastPosition = Vector3.Distance(transform.position, lastPosition);
+
+		AddReward(distanceFromPrey * 0.0001f); // Reward for approaching prey
+		
+		// Penalize staying still
+		if (distanceFromLastPosition < minDistanceMoved)
+			AddReward(-0.005f);
+		
+		// Give a small positive reward for moving a certain distance.
+		if (distanceFromLastPosition > minDistanceMoved)
+			AddReward(0.005f);
+
+		lastPosition = transform.position;
+
+		//Debug.Log($"Distance from Last Position: {distanceFromLastPosition} || Distance fromPredator: {distanceToPredator}" );
 	}
 
 
+	/// FOR TESTING
+	//public override void Heuristic(in ActionBuffers actionsOut) {
+	//	ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+	//	continuousActions[0] = Input.GetAxisRaw("Horizontal");
+	//	continuousActions[1] = Input.GetAxisRaw("Vertical");
+	//}
 }
--- Assets/Scripts/Prey.cs
+++ Assets/Scripts/Prey.cs
@@ -1,8 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 using UnityEngine;
 
-public class Prey : MonoBehaviour {
+public class Prey : Agent {
 
+	[SerializeField] Predator predator;
+	[SerializeField] float moveSpeed = 1f;
+	[SerializeField] float minDistanceMoved = 0.1f;
+	[SerializeField] Transform spawnArea;
+
+	Vector3 lastPosition = new Vector3();
+
+	public BoxCollider captureCollider;
+
+	public override void OnEpisodeBegin() => transform.localPosition = GetInitialPosition(spawnArea);
+
+	Vector3 GetInitialPosition(Transform spawnArea) {
+		var rndSpawnBounds = new Vector3(Random.Range((-spawnArea.localScale.x / 2), spawnArea.localScale.x / 2), 0, Random.Range((-spawnArea.localScale.z / 2), spawnArea.localScale.z / 2));
+		return spawnArea.localPosition + rndSpawnBounds;
+	}
+
+	public override void CollectObservations(VectorSensor sensor) {
+		sensor.AddObservation(transform.localPosition);
+		sensor.AddObservation(predator.transform.localPosition);
+	}
+
+	public override void OnActionReceived(ActionBuffers actions) {
+
+		var x = actions.ContinuousActions[0];
+		var z = actions.ContinuousActions[1];
+		var moveDirection = new Vector3(x, 0, z);
+
+		transform.localPosition += moveDirection * Time.deltaTime * moveSpeed;
+
+		RewardEvasion();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.layer == LayerMask.NameToLayer("Wall")) {
+			AddReward(-0.8f);
+			Debug.Log("prey hit wall");
+			EndEpisode();
+			//OnWallCollided(); 
+		}
+	}
+
+	public void OnCaptured() => AddReward(-1f);
+
+	public void OnEvaded() {
+		AddReward(1f);
+		Debug.Log("safely evaded :D ");
+	}
+
+	void RewardEvasion() {
+		var distanceToPredator = Vector3.Distance(transform.position, predator.transform.position); // Calculate distance to predator
+		var distanceFromLastPosition = Vector3.Distance(transform.position, lastPosition);
+
+		AddReward(distanceToPredator * 0.001f); // Reward for staying away from predator
+		// Penalize staying still
+		if (distanceFromLastPosition < minDistanceMoved)
+			AddReward(-0.01f);
+
+		// Give the prey a small positive reward for moving a certain distance or for exploring new areas
+		if (distanceFromLastPosition > minDistanceMoved)
+			AddReward(0.01f);
+
+		float survivalReward = 0.01f; // Small positive reward per time step
+		AddReward(survivalReward);
+
+		lastPosition = transform.position;
+
+		//Debug.Log($"Distance from Last Position: {distanceFromLastPosition} || Distance fromPredator: {distanceToPredator}" );
+	}
+
+	/// FOR TESTING
+	//public override void Heuristic(in ActionBuffers actionsOut) {
+	//	ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+	//	continuousActions[0] = Input.GetAxisRaw("Horizontal");
+	//	continuousActions[1] = Input.GetAxisRaw("Vertical");
+	//}
 
 }
