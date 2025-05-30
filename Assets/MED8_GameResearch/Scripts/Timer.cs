87fb8c7ca8ec8cee0092970832b548345364d6ae
virimine <virimine@gmail.com>
2025-05-30 18:45:41 +0200

    timer animation

--- Assets/MED8_GameResearch/Scripts/Timer.cs
+++ Assets/MED8_GameResearch/Scripts/Timer.cs
@@ -6,7 +6,12 @@ using UnityEngine;
 public class Timer : MonoBehaviour {
 
 	[SerializeField] TextMeshProUGUI displayTMP;
+	[SerializeField] Animator animator;
 	[SerializeField] float totalTime = 60f;
+
+	string triggerName = "StartAnim";
+	float triggerTime = 10f;
+	 bool hasTriggered;
 
 	float remainingTime;
 	bool running;
@@ -20,23 +25,43 @@ public class Timer : MonoBehaviour {
 		UpdateTimerDisplay(remainingTime);
 
 		remainingTime -= Time.deltaTime;
+
+		PlayLowTimeAnimation();
+
 		if (remainingTime <= 0f) {
-			running = false;
 			onTimerFinished?.Invoke();
+			ResetTimer();
 		}
 	}
 
 	public float AddDebugTime() => totalTime += 3000;
 
 	public void StartTimer(System.Action callback) {
+
+		running = true;
+
 		remainingTime = totalTime;
 		onTimerFinished = callback;
-		running = true;
 	}
 
 	public void UpdateTimerDisplay(float timeInSeconds) {
 		int minutes = Mathf.FloorToInt(timeInSeconds / 60f);
 		int seconds = Mathf.FloorToInt(timeInSeconds % 60f);
 		displayTMP.text = $"time: {minutes:00}:{seconds:00}";
+	}
+
+	void PlayLowTimeAnimation(){
+
+		if (!hasTriggered && remainingTime <= triggerTime) {
+
+			animator.SetTrigger(triggerName);
+			hasTriggered = true;
+		}
+	}
+
+	void ResetTimer(){
+		hasTriggered = false;
+		running = false;
+		animator.Play("Default");
 	}
 }
